dc62fa72ae58aa228abd01703a25511f0ec387fa
Nicholas Letarte <110304219+nicholasletarte@users.noreply.github.com>
2023-11-13 16:48:02 -0500

    Update HeroAttack.java

--- battleCode/HeroAttack.java
+++ battleCode/HeroAttack.java
@@ -1,8 +1,3 @@
-/******
- Name: Michael Swartz & Nicholas Letarte
- Assignment: Creative Lab
- Date: 10/31/2022
- ******/
 public enum HeroAttack {
     ATTACK, ATTACK_WITH_WEAPON, SPEC_ATTACK_WITH_WEAPON;
 }
